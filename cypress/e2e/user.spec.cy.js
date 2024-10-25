134947da60b58e5fb12735dcf0041fb4edb663d5
Luany Ramiro <luanyramiro@yahoo.com.br>
2024-10-25 11:17:50 -0300

    Add My Info Page

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,55 +2,32 @@ import userData from '../fixtures/users/user-data.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
+import MyInfoPage from '../pages/myInfoPage'
 
 
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
+const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
 
-  const selectorList = {
-   
-    firstNameField: "[name='firstName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    dateField: "[placeholder='yyyy-dd-mm']",
-    genericComboBox: ".oxd-select-text",
-    secondItemComboBox: ".oxd-select-dropdown > :nth-child(2)",
-    forthItemComboBox: ".oxd-select-dropdown > :nth-child(4)",
-    dataCloseButton: ".--close",
-    submitButton: "[type='submit']"
-
-  } 
-
-  it.only('User Info Update - Success', () => {
+  it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password)
     dashboardPage.checkdashboardPage()
     menuPage.accessMyInfo()
-    cy.get(selectorList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorList.genericField).eq(3).clear().type('EmpIdTest')
-    cy.get(selectorList.genericField).eq(4).clear().type('OtherIdTest')
-    cy.get(selectorList.genericField).eq(5).clear().type('DrivesLicenseNumber')
-    cy.get(selectorList.genericField).eq(6).clear().type('2025-03-10')
-    cy.get(selectorList.dataCloseButton).click()
-    cy.get(selectorList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get(selectorList.genericComboBox).eq(0).click({ force: true })
-    cy.get(selectorList.secondItemComboBox).click()
-    cy.get(selectorList.genericComboBox).eq(1).click({ force: true })
-    cy.get(selectorList.forthItemComboBox).click()
-    menuPage.accessPerformance()
+    //menuPage.accessPerformance() e outros
+    myInfoPage.fillPersonalDetails('firstName', 'lastName')
+    myInfoPage.fillEmployeeDetails('empId', 'otherId', 'Drivers number', '2024-10-24')
+    myInfoPage.fillStatus()
+    myInfoPage.saveForm()
 
   })
-  
+
   it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userFail.password)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongcredentialAlert)
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password)
+    loginPage.checkAccessInvalid()
   })
 })
--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,6 +1,6 @@
 class LoginPage {
 
-    selectorList() {
+    selectorsList() {
         const selectors = {
             usernameField: "[name='username']",
             passwordField: "[name= 'password']",
@@ -17,11 +17,14 @@ class LoginPage {
 }
 
     loginWithUser (username, password) {
-        cy.get(this.selectorList().usernameField).type(username)
-        cy.get(this.selectorList().passwordField).type(password)
-        cy.get(this.selectorList().loginButton).click()
-    }
+        cy.get(this.selectorsList().usernameField).type(username)
+        cy.get(this.selectorsList().passwordField).type(password)
+        cy.get(this.selectorsList().loginButton).click()
+}
 
+    checkAccessInvalid () {
+        cy.get(this.selectorsList().wrongcredentialAlert)
+}
 
 }
 
